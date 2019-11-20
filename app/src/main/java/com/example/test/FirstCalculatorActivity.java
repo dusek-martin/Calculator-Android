40edb92045e8daecd69f11d281f10a2b93ec3b1a
Martin Dušek <Dusek.Martin92@gmail.com>
2019-11-20 18:02:44 +0100

    Added activity for calculator

---
+++ app/src/main/java/com/example/test/FirstCalculatorActivity.java
@@ -0,0 +1,52 @@
+package com.example.test;
+
+public class FirstCalculatorActivity {
+
+    public FirstCalculatorActivity(){
+
+    }
+
+    public void writeNumber(){
+
+    }
+
+    public void writeComma(){
+
+    }
+
+    public void clearAll(){
+
+    }
+
+    public void clearLast(){
+
+    }
+
+    public void makeNegative(){
+
+    }
+
+    public void sqrRootOfNumber(){
+
+    }
+
+    public void add(){
+
+    }
+
+    public void subtract(){
+
+    }
+
+    public void multiple(){
+
+    }
+
+    public void divide(){
+
+    }
+
+    public void calculate(){
+
+    }
+}
